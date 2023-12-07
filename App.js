d2525ec9a6ac2a04fdc2275b3a9e44b9699e84ec
RubTheKid <jadercardoso@tuta.io>
2023-12-07 11:49:57 -0300

    adding kayboard avoiding view (ios & android)

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { KeyboardAvoidingView, Platform } from 'react-native';
 
 import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
@@ -20,10 +20,16 @@ export default function App() {
 
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator>
-            <Stack.Screen name='HomeScreen' component={HomeScreen} options={{headerShown: false }}/>
-            <Stack.Screen name='MapScreen' component={MapScreen} options={{headerShown: false }}/>
-          </Stack.Navigator>
+          <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={{ flex: 1 }}
+          kayboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen name='HomeScreen' component={HomeScreen} options={{headerShown: false }}/>
+              <Stack.Screen name='MapScreen' component={MapScreen} options={{headerShown: false }}/>
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </NavigationContainer>
 
